eda808451a6ec6530139d028cea7654223ed4d44
JekugaBrandon <jekugabrandon8@gmail.com>
2023-09-08 10:21:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main c program with output of puts as function
+ * Return: 0(sucessfull)
+ */
+int main(void)
+{
+puts{"\"Programming is like building a multilingual puzzle"};
+};
+return (0);
