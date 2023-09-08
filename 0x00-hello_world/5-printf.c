b0379ca0eddc3b1aa7db88d7e42a72782bfd2ec3
JekugaBrandon <jekugabrandon8@gmail.com>
2023-09-08 10:50:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Writing a program that pint a value passing the printf function
+ * Return:0 (as succided)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,"\n);
+return (0);
+}
